fffcbc7957bd97546eb98612a61173b867c76017
AlexLionP <alejandrodeleon904@gmail.com>
2024-02-29 06:12:16 -0600

    Se modifico el modelo Order y User y se agregaron los servicios FabricOrder y OrderService

---
+++ src/main/java/com/tecnomexcoders/cakecode/service/FabricOrderService.java
@@ -0,0 +1,30 @@
+package com.tecnomexcoders.cakecode.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+
+import com.tecnomexcoders.cakecode.dto.OrderDTO;
+
+import com.tecnomexcoders.cakecode.model.Order;
+
+@Service
+public class FabricOrderService {
+	
+	public Order crearOrder(OrderDTO orderDTO) {
+		return new Order(orderDTO);
+	}
+	
+	public OrderDTO crearOrderDTO(Order order) {
+		return new OrderDTO(order);
+	}
+	
+	public List<OrderDTO>crearOrdersDTO(List<Order>listOrders){
+		List<OrderDTO>listOrdersDTO = new ArrayList<>();
+		listOrders.stream().forEach(order->listOrdersDTO.add(crearOrderDTO(order)));
+		return listOrdersDTO;
+	}
+
+}
