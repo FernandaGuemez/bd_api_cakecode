a869afd404a1186cf0e34890572672625445e5d2
BryanTevillo <42023490+BryanTevillo@users.noreply.github.com>
2024-02-29 06:00:49 -0600

    Se agrega FabricdesignService y designservice tambien fabrictopperservice y topperservice

---
+++ src/main/java/com/tecnomexcoders/cakecode/service/FabricDesignService.java
@@ -0,0 +1,26 @@
+package com.tecnomexcoders.cakecode.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.tecnomexcoders.cakecode.dto.DesignDTO;
+import com.tecnomexcoders.cakecode.model.Design;
+
+@Service
+public class FabricDesignService {
+
+    public Design createDesign(DesignDTO designDTO) {
+        return new Design(designDTO);
+    }
+
+    public DesignDTO createDesignDTO(Design design) {
+        return new DesignDTO(design);
+    }
+
+    public List<DesignDTO> createDesignsDTO(List<Design> designs) {
+        List<DesignDTO> designsDTO = new ArrayList<>();
+        designs.forEach(design -> designsDTO.add(new DesignDTO(design)));
+        return designsDTO;
+    }
+
+}
