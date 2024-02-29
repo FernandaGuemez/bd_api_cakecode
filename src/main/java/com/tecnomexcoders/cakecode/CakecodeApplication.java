934efad38eb81ebc04b3a63a04d9fed9a9245f2c
FernandaG <fernanda.guemez@gmail.com>
2024-02-28 23:14:28 -0600

    Se agregan package y se configura la conexion con la bd de mysql

---
+++ src/main/java/com/tecnomexcoders/cakecode/CakecodeApplication.java
@@ -0,0 +1,13 @@
+package com.tecnomexcoders.cakecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakecodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecnomexcoders/cakecode/CakecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnomexcoders.cakecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
