68f9bdf559c590a87df065df571edc5603c8f7fe
FernandaG <fernanda.guemez@gmail.com>
2024-02-29 02:07:48 -0600

    Se agrega tabla de flavor product y toppings1

---
+++ src/main/java/com/tecnomexcoders/cakecode/model/Flavor.java
@@ -0,0 +1,26 @@
+package com.tecnomexcoders.cakecode.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "flavor")
+public class Flavor {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "flavor_id")
+	private Integer id;
+	
+	@Column(length = 100)	
+	private String flavor;
+
+}
