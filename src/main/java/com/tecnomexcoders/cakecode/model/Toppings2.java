1e00354f5b6ad9a9769a3aad66527eb24a832b07
ItzelPr03 <itzelperez.rq@gmail.com>
2024-02-29 02:10:03 -0600

    agregamos modelos de CustomCake, Design,Topper,Toppings2

---
+++ src/main/java/com/tecnomexcoders/cakecode/model/Toppings2.java
@@ -0,0 +1,29 @@
+package com.tecnomexcoders.cakecode.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name="toppings2")
+public class Toppings2 {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="toppings2_id")
+	private Integer id;
+	
+	@Column(name="topping",length=150)
+	private String toppingsImage;
+	
+	@Column(length=45)
+	private String description;
+	
+}
