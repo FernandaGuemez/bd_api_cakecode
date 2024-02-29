a62f814767ca9674ba4565b0c48ded782485f1f5
ItzelPr03 <itzelperez.rq@gmail.com>
2024-02-29 03:02:17 -0600

    agregamos fk en customcake

--- src/main/java/com/tecnomexcoders/cakecode/model/CustomCake.java
+++ src/main/java/com/tecnomexcoders/cakecode/model/CustomCake.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,6 +23,25 @@ public class CustomCake {
 	private Integer id;
 	@Column(name="card",length=150)
 	private String card;
+	
+	@ManyToOne
+	@JoinColumn(name="fk_size_id")
+	private Size size;
+	@ManyToOne
+	@JoinColumn(name="fk_flavor_id")
+	private Flavor flavor;
+	@ManyToOne
+	@JoinColumn(name="fk_toppings1_id")
+	private Toppings1 toppings1;
+	@ManyToOne
+	@JoinColumn(name="fk_toppings2_id")
+	private Toppings2 toppings2;
+	@ManyToOne
+	@JoinColumn(name="fk_topper_id")
+	private Topper topper;
+	@ManyToOne
+	@JoinColumn(name="fk_design_id")
+	private Design design;
 	
 	
 	
