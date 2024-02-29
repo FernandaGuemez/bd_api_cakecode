961557dfc7b73da4f6b5716364559fa9ee8a5247
ItzelPr03 <itzelperez.rq@gmail.com>
2024-02-29 03:29:41 -0600

    agregamos repository de toppings 1 y 2

---
+++ src/main/java/com/tecnomexcoders/cakecode/repository/Toppings1Repository.java
@@ -0,0 +1,11 @@
+package com.tecnomexcoders.cakecode.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tecnomexcoders.cakecode.model.Toppings1;
+
+@Repository
+public interface Toppings1Repository extends JpaRepository<Toppings1, Integer> {
+
+}
---
+++ src/main/java/com/tecnomexcoders/cakecode/repository/Toppings2Repository.java
@@ -0,0 +1,12 @@
+package com.tecnomexcoders.cakecode.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.tecnomexcoders.cakecode.model.Toppings2;
+
+@Repository
+public interface Toppings2Repository extends JpaRepository<Toppings2, Integer> {
+
+}
+
+
